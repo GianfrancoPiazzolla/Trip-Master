b10c8e27a406f13587d4b14d83ed890f0c9db4a4
GianfrancoPiazzolla <gianfranco.piazzolla@gmail.com>
2026-02-05 16:01:06 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,31 +1,37 @@
-const CACHE_NAME = 'trip-master-cache-v1';
-const urlsToCache = [
-  '/',
-  'index.html',
-  'favicon.ico',
-  'icon-192x192.png',
-  'icon-512x512.png'
+const CACHE_NAME = 'trip-master-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  'https://cdn.jsdelivr.net/npm/chart.js',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js',
+  'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png'
 ];
 
-self.addEventListener('install', event => {
+// Installazione: salvataggio file in cache
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
   );
 });
 
-self.addEventListener('fetch', event => {
+// Attivazione e pulizia vecchie cache
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)));
+    })
+  );
+});
+
+// Strategia di fetch: Network First, falling back to cache
+// Fondamentale per le mappe e i dati in tempo reale
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
   );
 });
